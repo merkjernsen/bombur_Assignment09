1674f2a91cbe56360c57b1ee4b3bc3a9df62f76b
bluejaeger <115363783+bluejaeger@users.noreply.github.com>
2022-10-22 11:56:29 -0400

    Initial Workspace

---
+++ bombur_Assignment09/ClassPackage/BaseballClasses.py
@@ -0,0 +1,10 @@
+'''
+Name: Kevin, Wilson, Mark Johnson, Brett Perez
+email: perezbd@mail.uc.edu
+Assignment: Assignment 09
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: In this project, we use Classes and OOP to create a simple, informational program.
+Citations: 
+Anything else that's relevant: 
+'''
